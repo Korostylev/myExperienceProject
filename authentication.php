610b6282a261e63c4b38fb5211c84179963ad9af
Korostylev <korostylevmaxim@mail.ru>
2024-04-03 10:24:50 +0300

    -

--- authentication.php
+++ authentication.php
@@ -3,6 +3,40 @@
     $userinfo='';
     $state='0';
 
+    if( (isset($_COOKIE['login'])) & (isset($_COOKIE['pass'])) ) {// если в куках лежит логин и зашифрованый пароля
+      if (!isset($_GET['exit'])) {// если кнопка выход не была нажата
+        $login=$_COOKIE['login'];
+        $pass=$_COOKIE['pass'];
+    
+        // проверяем наличие пользователя в БД и достаём оттуда пароль
+        $sql="SELECT * FROM `users` WHERE `login`='$login'";
+        $res=mysqli_query($connected, $sql);
+        if(mysqli_num_rows($res)){// если пользователь есть в БД
+          $userinfo = mysqli_fetch_array($res);// в этой переменной лежит пароль из БД
+          if(strcmp($pass,$userinfo['pass']) == 0) { //проверяем схожесть пароля из БД с паролем из куков
+            $time=time();
+            // устанавливаем куки для запоминания статуса пользователя
+            setcookie("login",$login,$time+36000);
+            setcookie("pass",$pass,$time+36000);
+            $state = 1;// статус, если 1, тогда пользователь авторизован
+          }
+          else {
+            echo 'error pass from cookie';
+            setcookie("login");
+            setcookie("pass");
+          }
+        }
+        else {
+          echo 'error login from cookie';
+          setcookie("login");
+          setcookie("pass");
+        }
+      } else {
+        //обнуляем куки, если была нажата кнопка выход
+        setcookie("login");
+        setcookie("pass");
+      }
+    }
     if($state != 1 & (isset($_POST['typeOperation'])) & $_POST['typeOperation'] == 'enter') {// если после проверки куков, оказалось, что пользователь не авторизован, то идем дальше
         if( (isset($_POST['login'])) & (isset($_POST['pass'])) ){ // если пользователь ввёл логин и пароль
           $login = $_POST['login'];	
