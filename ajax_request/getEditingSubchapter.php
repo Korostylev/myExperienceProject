b8e107fd42c79e8301c60c5c82cde92f0e4b3025
Korostylev <korostylevmaxim@mail.ru>
2024-04-17 13:31:34 +0300

    -

--- ajax_request/getEditingSubchapter.php
+++ ajax_request/getEditingSubchapter.php
@@ -28,6 +28,13 @@
       }
     }
     else {
+      $chapter_id = $_GET['chapterid'];
+      $color = "#bbbbbb";
+      $resultChapter = mysqli_query($connected, "SELECT * FROM `main_chapters` WHERE `id`='$chapter_id'");
+      if(mysqli_num_rows($resultChapter)){
+        $chapterElement = mysqli_fetch_array($resultChapter);
+        $color = $chapterElement['color'];
+      }
       echo '<div class="title-window">Добавить элемент</div>
       <form action="/index.php" >
         <div class="form-element"><input id="subchapter_title" type="text" name="title" placeholder="Заголовок" /></div>
@@ -35,10 +42,10 @@
           <textarea id="subchapter_description" name="description" placeholder="Описание" maxlength="254" ></textarea>
         </div>
         <div class="form-element">
-          <input id="subchapter_color" type="color" name="color" placeholder="Цвет" style="margin-right: 40px; height: 32px; padding: 0px;" />
+          <input id="subchapter_color" type="color" name="color" placeholder="Цвет" style="margin-right: 40px; height: 32px; padding: 0px;" value="',$color,'" />
           <input class="button" type="button" value="Добавить"  onclick="addSubchapter()"/>
           <input id="subchapter_id" type="hidden" name="id" value="0" />
-          <input id="subchapter_chapter_id" type="hidden" name="chapter_id" value="0" />
+          <input id="subchapter_chapter_id" type="hidden" name="chapter_id" value="',$chapter_id,'" />
         </div>
       </form>';
     }
